4432c6da29b0c87a5ef2aa52bef6127e52ab814c
Daniel K <da.ka@freenet.de>
2020-12-26 23:07:57 +0100

    Added Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material-module';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { LoginComponent } from './components/login/login.component';
+import { GoalsComponent } from './components/goals/goals.component';
+import { UserOverviewComponent } from './components/user-overview/user-overview.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavigationBarComponent, LoginComponent, GoalsComponent, UserOverviewComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
