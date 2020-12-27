b650fa86e0b7d53c1c5576e2a4dc267a46fec7c5
Daniel K <da.ka@freenet.de>
2020-12-27 02:20:05 +0100

    Added Dialog

--- src/app/components/goals/goals.component.ts
+++ src/app/components/goals/goals.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GoalEditDialogComponent } from '../goal-edit-dialog/goal-edit-dialog.component';
+import {MatDialog,} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-goals',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./goals.component.scss']
 })
 export class GoalsComponent implements OnInit {
+  
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
+
+
+    const dialogRef = this.dialog.open(GoalEditDialogComponent, {
+      
+    });
+
   }
 
 }
