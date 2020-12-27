0c9bd672ede3bf8ca97d03307685245874161510
Sebastian Espei <seblsebastian@aol.de>
2020-12-27 23:18:20 +0100

    Add missig goal service methods

--- src/app/services/goal-service.service.ts
+++ src/app/services/goal-service.service.ts
@@ -3,12 +3,16 @@ import { Goal } from '../entities/goal';
 import { User } from '../entities/user';
 import { BackendService } from './backend.service';
 import { IdService } from './id.service';
+import { SubGoal } from '../entities/sub-goal';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GoalServiceService {
-  constructor(private backendService: BackendService, private idService: IdService) {}
+  constructor(
+    private backendService: BackendService,
+    private idService: IdService
+  ) {}
 
   public getGoalFromUser(user: User): Goal[] {
     return user.scopeList == null ? [] : user.scopeList;
@@ -27,16 +31,34 @@ export class GoalServiceService {
     return newGoal;
   }
 
-  public createGoal(user: User, name: string): Goal {
-    const newGoal: Goal = {
-      id: this.idService.getNewGoalId(user.scopeList, user),
+  public createSubGoal(goal: Goal, user: User, name: string): SubGoal {
+    const newGoal: SubGoal = {
+      id: this.idService.getNewSubGoalId(goal.subscope, goal, user),
       name,
-      subscope: [],
+      checked: false,
     };
 
-    user.scopeList.push(newGoal);
+    goal.subscope.push(newGoal);
     this.backendService.updateUser(user);
 
     return newGoal;
+  }
+
+  public modifyGoal(goal: Goal, user: User): void {
+    this.backendService.updateUser(user);
+  }
+
+  public modifySubGoal(subGoal: SubGoal, user: User): void {
+    this.backendService.updateUser(user);
+  }
+
+  public deleteGoal(goal: Goal, user: User): void {
+    user.scopeList = user.scopeList.filter((item) => item.id !== goal.id);
+    this.backendService.updateUser(user);
+  }
+
+  public deleteSubGoal(subGoal: SubGoal, goal: Goal, user: User): void {
+    goal.subscope = goal.subscope.filter((item) => item.id !== subGoal.id);
+    this.backendService.updateUser(user);
   }
 }
