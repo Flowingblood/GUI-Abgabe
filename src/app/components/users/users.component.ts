327f85d0f46d9b8c6c45f331b841a8d8cf2dc5db
Daniel K <da.ka@freenet.de>
2020-12-28 00:20:48 +0100

    Added Users Component Stuff

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { User } from 'src/app/entities/user';
+import { BackendService } from 'src/app/services/backend.service';
 
 @Component({
   selector: 'app-users',
@@ -9,11 +10,17 @@ import { User } from 'src/app/entities/user';
 })
 export class UsersComponent implements OnInit {
 
+  displayedColumns: string[] = ['id','username', 'firstname', 'lastname', 'actions'];
+
   users : MatTableDataSource<User>;
 
-  constructor() { }
+  constructor(public backendService: BackendService) { }
 
   ngOnInit(): void {
+
+    
+    this.backendService.getAllUser().then((u) => {this.users = new MatTableDataSource(u); console.log(this.users.data);})
+    
   }
 
 }
