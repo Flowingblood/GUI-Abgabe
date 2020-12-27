9194cdd8a27ab33ea17a1168903b0ecb6abafbee
Sebastian Espei <seblsebastian@aol.de>
2020-12-28 00:50:28 +0100

    Add user service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { User } from '../entities/user';
+import { IdService } from './id.service';
+import { BackendService } from './backend.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  constructor(
+    private idService: IdService,
+    private backendService: BackendService
+  ) {}
+
+  public getAllUser(): Promise<User[]> {
+    return this.backendService.getAllUser();
+  }
+
+  public async createUser(user: User): Promise<User> {
+    user.scopeList = [];
+    user.id = this.idService.getNewUserId(await this.getAllUser());
+    this.backendService.createUser(user);
+    return user;
+  }
+
+  public modifyUser(user: User): void {
+    this.backendService.updateUser(user);
+  }
+
+  public delete(user: User): void {
+    this.backendService.deleteUser(user);
+  }
+}
