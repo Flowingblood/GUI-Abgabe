ce456732bebd65688c501d340baaed0d21f15a80
Sebastian Espei <seblsebastian@aol.de>
2020-12-27 23:22:07 +0100

    Remove testing code

--- src/app/components/goals/goals.component.ts
+++ src/app/components/goals/goals.component.ts
@@ -12,12 +12,11 @@ import { AuthorizationService } from 'src/app/services/authorization.service';
 @Component({
   selector: 'app-goals',
   templateUrl: './goals.component.html',
-  styleUrls: ['./goals.component.scss']
+  styleUrls: ['./goals.component.scss'],
 })
 export class GoalsComponent implements OnInit {
-
   currentUser: User;
-  
+
   // Edit Popupdialog
   editGoalDialogRef: MatDialogRef<GoalEditDialogComponent>;
 
@@ -29,47 +28,41 @@ export class GoalsComponent implements OnInit {
 
   goals: Goal[];
 
+  constructor(
+    public goalEditDialog: MatDialog,
+    public addGoalDialog: MatDialog,
+    public deleteGoalDialog: MatDialog,
+    private backendService: BackendService,
+    public authService: AuthorizationService
+  ) {}
 
-  constructor(public goalEditDialog: MatDialog, public addGoalDialog: MatDialog, public deleteGoalDialog: MatDialog,
-     private backendService: BackendService, public authService: AuthorizationService) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-    this.authService.getLoggedInUser().subscribe((currUser) => this.currentUser = currUser);
-    
-    this.goals = [{id: 1, name: "test", subscope:[{id: 10, name: "sub_test"}]} ,{id: 2, name: "test2"},{id: 3, name: "test3"}];
-    //this.goals = this.backendService.
-    /*const dialogRef = this.dialog.open(UserEditDialogComponent, {
-      
-    });*/
-
-  }
-
-  openEditDialog($event: any, goal: Goal): void{
+  openEditDialog($event: any, goal: Goal): void {
     $event.stopPropagation();
     this.editGoalDialogRef = this.goalEditDialog.open(GoalEditDialogComponent);
     this.editGoalDialogRef.componentInstance.goal = goal;
-    this.editGoalDialogRef.afterClosed().subscribe(result => {
+    this.editGoalDialogRef.afterClosed().subscribe((result) => {
       //TODO Update and database
     });
   }
 
-  openAddGoalDialog($event: any): void{
+  openAddGoalDialog($event: any): void {
     $event.stopPropagation();
     this.addGoalDialogRef = this.goalEditDialog.open(GoalAddDialogComponent);
-    this.addGoalDialogRef.afterClosed().subscribe(result => {
+    this.addGoalDialogRef.afterClosed().subscribe((result) => {
       //TODO Update and database
     });
-
   }
 
-  openDeleteGoalDialog($event: any, goal: Goal): void{
+  openDeleteGoalDialog($event: any, goal: Goal): void {
     $event.stopPropagation();
-    this.deleteGoalDialogRef = this.goalEditDialog.open(GoalDeleteDialogComponent);
+    this.deleteGoalDialogRef = this.goalEditDialog.open(
+      GoalDeleteDialogComponent
+    );
     this.deleteGoalDialogRef.componentInstance.goal = goal;
-    this.deleteGoalDialogRef.afterClosed().subscribe(result => {
+    this.deleteGoalDialogRef.afterClosed().subscribe((result) => {
       //TODO Update and database
     });
-
   }
-
 }
